59a5757c9942fdb98f84d8590d21f0949ee4beb5
deborahvp12 <deborahvillicana@outlook.com>
2026-03-11 22:01:12 -0600

    Sección de Alrededor del mundo - LazyVerticalGrid

--- app/src/main/java/com/example/t4_p1/MainActivity.kt
+++ app/src/main/java/com/example/t4_p1/MainActivity.kt
@@ -16,6 +16,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -35,6 +38,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.Image
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.layout.ContentScale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,15 +58,22 @@ class MainActivity : ComponentActivity() {
 
 data class News(
     val title: String,
-    val date: String
+    val date: String,
+    val image: Int
 )
 
 val latestNews = listOf(
-    News("El presidente de EE.UU. no muestra signos de arrepentimiento...", "febrero 08 - 2024"),
-    News("Bañarse en la piscina del desierto de Cleopatra", "febrero 10 - 2024"),
-    News("Gigantes tecnológicos apuestan por la IA", "febrero 12 - 2024")
+    News("El presidente de EE.UU. no muestra signos de arrepentimiento...", "febrero 08 - 2024", 1),
+    News("Bañarse en la piscina del desierto de Cleopatra", "febrero 10 - 2024", 1),
+    News("Gigantes tecnológicos apuestan por la IA", "febrero 12 - 2024", 1)
 )
 
+val worldNews = listOf(
+    News("El presidente de EE.UU. no muestra signos de arrepentimiento...", "", R.drawable.trump),
+    News("Bañarse en la piscina del desierto de Cleopatra", "", R.drawable.cleo),
+    News("Gigantes tecnológicos apuestan por la IA", "", R.drawable.tech),
+    News("El rover de Marte envía nuevas imágenes", "", R.drawable.marte)
+)
 @Composable
 fun NewsCard(news: News){
     Box(
@@ -87,6 +100,35 @@ fun NewsCard(news: News){
                 color = Color.White,
                 fontSize = 15.sp
             )
+        }
+    }
+}
+
+@Composable
+fun WorldNewsCard(news: News){
+    Box(
+        modifier = Modifier
+            .height(200.dp)
+            .clip(RoundedCornerShape(24.dp))
+            .background(Color(0xFF6C63FF))
+    ){
+        Image(
+            painter = painterResource(id =news.image),
+            contentDescription = news.title,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+        Box(
+            modifier = Modifier
+                .clip(RoundedCornerShape(24.dp))
+                .background(Color.LightGray)
+                .align(Alignment.BottomStart)
+                .fillMaxWidth()
+
+        ){
+            Text(text = news.title,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(7.dp))
         }
     }
 }
@@ -165,6 +207,25 @@ fun HomeScreen(){
             contentPadding = PaddingValues(horizontal = 20.dp)
         ) {
             items(latestNews) {news -> NewsCard(news = news)}
+        }
+        Text(
+            text = "Alrededor del mundo",
+            fontWeight = FontWeight.Bold,
+            fontSize = 25.sp,
+            modifier = Modifier.padding(20.dp)
+        )
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(420.dp),
+            contentPadding = PaddingValues(horizontal = 20.dp),
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            items(worldNews){
+                news -> WorldNewsCard(news = news)
+            }
         }
     }
 }
